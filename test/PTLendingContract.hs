84d514b03f9299b9dfb5c1f5f7a0d941295d63b9
congle-engineer <congle-engineer@gmail.com>
2023-08-09 15:34:13 +0700

    Remove all warning messages

--- test/PTLendingContract.hs
+++ test/PTLendingContract.hs
@@ -17,10 +17,10 @@ import           GeneralParams
 import           Utility
 import           CreditScoringContract      as CS
 import           LendingContract            as LC
-import           PlutusTx.Prelude           (find, isJust, fromMaybe)
-import qualified Plutus.V2.Ledger.Api       as PlutusV2
-import qualified Plutus.V2.Ledger.Contexts  as PlutusV2
-import qualified PlutusTx
+import           PlutusTx.Prelude           (find, isJust)
+-- import qualified Plutus.V2.Ledger.Api       as PlutusV2
+-- import qualified Plutus.V2.Ledger.Contexts  as PlutusV2
+-- import qualified PlutusTx
 
 ---------------------------------------------------------------------------------------------------
 --------------------------------------- TESTING MAIN ----------------------------------------------
@@ -104,35 +104,35 @@ createLendingPackages usp packageNumber val pkhOperator valOperatorToken = mconc
   , payToKey pkhOperator valOperatorToken
   ]
 
-borrowPackage :: UserSpend -> PubKeyHash -> Integer -> Value -> Value -> Tx
-borrowPackage usp user packageNumber valPackage valScoringNFT = mconcat
+borrowPackage :: UserSpend -> PubKeyHash -> Integer -> Value -> Integer -> Value -> Tx
+borrowPackage usp user packageNumber valPackage userScore valScoringNFT = mconcat
   [ userSpend usp
-  , payToScript scriptLendingContract (HashDatum (NFTInfo 1000 user 1)) valScoringNFT
+  , payToScript scriptLendingContract (HashDatum (NFTInfo userScore user packageNumber)) valScoringNFT
   , payToKey user valPackage
   ]
 
 ---------------------------------------------------------------------------------------------------
 ------------------------------------- TESTING PROPERTIES ------------------------------------------
 
-prop_Borrow_NoScoringNFTInInputs_Fails :: Bool -> Bool -> Property
-prop_Borrow_NoScoringNFTInInputs_Fails canBorrow isScoringNFTOwner =
+prop_Borrow_NoScoringNFTInInputs_Fails :: Bool -> Integer -> Property
+prop_Borrow_NoScoringNFTInInputs_Fails isScoringNFTOwner userScore =
   (isScoringNFTOwner == False) ==> 
-    runChecks False isScoringNFTOwner
+    runChecks False isScoringNFTOwner userScore
 
 ---------------------------------------------------------------------------------------------------
 ------------------------------------- RUNNING THE TESTS -------------------------------------------
 
-runChecks :: Bool -> Bool -> Property
-runChecks canBorrow isScoringNFTOwner =
-  collect (isScoringNFTOwner) $ monadic property check
+runChecks :: Bool -> Bool -> Integer -> Property
+runChecks canBorrow isScoringNFTOwner userScore =
+  collect (isScoringNFTOwner, userScore) $ monadic property check
   -- monadic property check
     where 
       check = do
-        isGood <- run $ testValues canBorrow isScoringNFTOwner
+        isGood <- run $ testValues canBorrow isScoringNFTOwner userScore
         assert isGood
 
-testValues :: Bool -> Bool -> Run Bool
-testValues canBorrow isScoringNFTOwner = do
+testValues :: Bool -> Bool -> Integer -> Run Bool
+testValues canBorrow isScoringNFTOwner userScore = do
   [operator, scoringNFTOwner, normalUser] <- setupUsers
 
   let normalVal     = adaValue 100 
@@ -144,41 +144,36 @@ testValues canBorrow isScoringNFTOwner = do
   submitTx operator tx
   waitNSlots 10
 
-  uspOperator <- spend operator operatorVal
+  uspOperator1 <- spend operator operatorVal
   let package1 = 1
   let amount1 = adaValue 1000
-  let tx1 = createLendingPackages uspOperator package1 amount1 operator operatorVal
+  let tx1 = createLendingPackages uspOperator1 package1 amount1 operator operatorVal
   submitTx operator tx1
   waitNSlots 10
 
-  uspOperator <- spend operator operatorVal
+  uspOperator2 <- spend operator operatorVal
   let package2 = 2
   let amount2 = adaValue 2000
-  let tx2 = createLendingPackages uspOperator package2 amount2 operator operatorVal
+  let tx2 = createLendingPackages uspOperator2 package2 amount2 operator operatorVal
   submitTx operator tx2
   waitNSlots 10
 
-  uspOperator <- spend operator operatorVal
+  uspOperator3 <- spend operator operatorVal
   let package3 = 3
   let amount3 = adaValue 1000
-  let tx3 = createLendingPackages uspOperator package3 amount3 operator operatorVal
+  let tx3 = createLendingPackages uspOperator3 package3 amount3 operator operatorVal
   submitTx operator tx3
   waitNSlots 10
 
   uspScoringNFTOwner <- spend scoringNFTOwner valScoringNFT
   uspNormalUser      <- spend normalUser normalVal
 
-  -- utxos <- utxoAt scoringNFTOwner
-  -- let utxoHasNFTInfo = find(\x@(_, txOut') -> isJust (txOutDatumHash txOut')) utxos
-  -- let [(txOutRef, txOut)] = utxoHasNFTInfo
-  -- let nftInfo = parseNFTInfo txOut
-
   let packageNumber = 1
   let amount = adaValue 1000
 
-  let usp  = if canBorrow then uspScoringNFTOwner else uspNormalUser
-      user = if canBorrow then scoringNFTOwner else normalUser
-      tx4  = borrowPackage usp user packageNumber amount valScoringNFT
+  let usp  = if isScoringNFTOwner then uspScoringNFTOwner else uspNormalUser
+      user = if isScoringNFTOwner then scoringNFTOwner else normalUser
+      tx4  = borrowPackage usp user packageNumber amount userScore valScoringNFT
 
   if canBorrow then submitTx user tx4 else mustFail . submitTx user $ tx4
 
